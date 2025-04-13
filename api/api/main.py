8c086e5c19ca402a1d66913ebb25f3957da23e31
Alec Jensen <59067840+alec-jensen@users.noreply.github.com>
2025-04-12 20:36:13 -0500

    ihatecors

--- api/api/main.py
+++ api/api/main.py
@@ -26,7 +26,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:4321/",
+        "http://localhost:4321",
     ],
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
